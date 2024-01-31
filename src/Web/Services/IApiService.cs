8f58d1fd6c6b39aaa5bee8580161c0b0860d9f00
Daniel Hedegaard <daniel201002@live.dk>
2024-01-31 09:10:59 +0000

    Merged PR 15: Updated APIService interface
    Updated APIService interface to have update tasks on folder and file
    
    Related work items: #78

--- src/Web/Services/IApiService.cs
+++ src/Web/Services/IApiService.cs
@@ -14,13 +14,14 @@ namespace Web.Services
         Task<List<VDSFile>> GetFileList(int userId);
         Task<bool> CreateFile(VDSFile file);
         Task<bool> DeleteFile(int fileId);
-
+        Task<bool> UpdateFile(VDSFile file);
 
         //folder
         Task<Folder> GetFolder(int userId); 
         Task<List<Folder>> GetFolderList(int userId);
         Task<bool> CreateFolder(Folder folder);
         Task<bool> DeleteFolder(int folderId);
+        Task<bool> UpdateFolder(Folder folder);
 
 
         //log
